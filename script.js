79f7d27bd7868ecfe351e4d1ca2d349bdf483359
Zamaria Winston <112509733+zswt64@users.noreply.github.com>
2022-10-10 13:01:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+let allMovies = [];
+
+//Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
+class Movie {
+	constructor(title, rating, haveWatched) {
+		this.title = title;
+		this.rating = rating;
+		this.haveWatched = haveWatched;
+	}
+}
+
+//add a movie OBJECT to the allMovies array
+let addMovie = (movie) => {
+	allMovies.push(movie);
+	console.log("A new movie is added");
+}
+
+//iterate through all elements of allMovies array
+//print out to console in a correct format
+//print out the total number of movies in allMovies array
+let printMovies = () => {
+	console.log("Printing all movies...");
+	for (let i = 0; i < allMovies.length; i++) {
+		console.log(allMovies[i].title + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].haveWatched);
+	}
+	console.log("You have " + allMovies.length + " movies in total");
+
+}
+
+
+//print out to console, only the movies that has a rating higher than rating(argument)
+//print out the total number of matches
+let highRatings = (rating) => {
+	console.log("printing movie that has a rating higher than " + rating);
+	var matches = 0;
+	for (let i = 0; i < allMovies.length; i++) {
+		if (allMovies[i].rating > rating) {
+			console.log(allMovies[i].title + " has a rating of " + allMovies[i].rating);
+			matches++;
+		}
+	}
+	console.log("In total, there are " + matches + " matches");
+
+}
+
+
+//Toggle the 'haveWatched' property of the specified movie 
+let changeWatched = (title) => {
+	console.log("changing the status of the movie...");
+	for (let i = 0; i < allMovies.length; i++) {
+		if (allMovies[i].title == title) {
+			if (allMovies[i].haveWatched) {
+				allMovies[i].haveWatched = false;
+			}else { 
+				allMovies[i].haveWatched = true;
+			}
+		}
+	}
+	
+}
+
+
+
+////////////////////////////////////////////////////////////
+//Test code - DO NOT DELETE OR EDIT
+let x = new Movie("Spiderman", 3, true);
+let y = new Movie("Citizen Kane", 4, false);
+let z = new Movie("Zootopia", 4.5, true);
+
+allMovies.push(x,y,z);
+
+console.log("----------------");
+console.log("running program......");
+console.log("----------------");
+printMovies();
+
+
+let movie1 = new Movie("Parasite", 2, false);
+
+
+console.log("----------------");
+addMovie(movie1);
+console.log("----------------");
+
+
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+highRatings(3.5);
